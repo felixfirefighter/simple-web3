83c6b20b8a37c7fb788b0b0edd0ff91b57a4514a
Felix Lee <leeyoongti@gmail.com>
2021-11-02 23:20:25 +0800

    Updates

---
+++ precache-manifest.0bb64db7ed5e71b606a047b1dc9bafa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16480030b6a411f87a3a76dca2d06cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "727dfa336d6ee5a034e8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "127cc96d87ba2292beeb",
+    "url": "/static/js/2.e62715b3.chunk.js"
+  },
+  {
+    "revision": "727dfa336d6ee5a034e8",
+    "url": "/static/js/main.fcebe859.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
