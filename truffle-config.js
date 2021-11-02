1fd849119a9b6512b4b0d426ef98a937135b3760
Felix Lee <leeyoongti@gmail.com>
2021-11-02 22:24:22 +0800

    Initial commit

---
+++ truffle-config.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const path = require("path");
+require('dotenv').config()
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      port: 8545
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  }
+};
